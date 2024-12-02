b2e5228589ea6b0e85c6d5d07df46539e6660cb9
MrCheeze446 <mrcheeze12345@gmail.com>
2024-12-02 13:13:07 -0700

    started on day 2

---
+++ Day 2/day_2.py
@@ -0,0 +1,17 @@
+def determine_safe(previous, current):
+    pass
+
+
+def parse_input(file):
+    for line in file:
+        nums = line.split()
+        for previous, current in zip(nums, nums[1:]):
+            pass
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
