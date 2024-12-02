d8a326c5e9ac8374ecd63bd7f965bb0e97c89d58
MrCheeze446 <mrcheeze12345@gmail.com>
2024-12-02 13:38:10 -0700

    day 2: part 1 done

--- Day 2/day_2.py
+++ Day 2/day_2.py
@@ -1,16 +1,40 @@
-def determine_safe(previous, current):
-    pass
+def check_difference(nums):
+    for previous, current in zip(nums, nums[1:]):
+        if not (1 <= abs(int(previous) - int(current)) <= 3):
+            return False
+
+    return True
+
+
+def all_dcreasing(nums):
+    for previous, current in zip(nums, nums[1:]):
+        if int(previous) > int(current):
+            return False
+    return True
+
+
+def all_increasing(nums):
+    for previous, current in zip(nums, nums[1:]):
+        if int(previous) < int(current):
+            return False
+    return True
 
 
 def parse_input(file):
-    for line in file:
-        nums = line.split()
-        for previous, current in zip(nums, nums[1:]):
-            pass
+    safe_vals = 0
+
+    with open(file) as f:
+        for line in f:
+            nums = line.split()
+            if all_increasing(nums) or all_dcreasing(nums):
+                if check_difference(nums):
+                    safe_vals += 1
+
+    return safe_vals
 
 
 def main():
-    print("hello world")
+    print(parse_input("Day 2/input"))
 
 
 if __name__ == "__main__":
