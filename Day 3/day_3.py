932d7eeca7f3a070dc53dd4761ff8985cebaf035
Anthony <mrcheeze12345@gmail.com>
2024-12-03 15:12:58 -0700

    regex soup

--- Day 3/day_3.py
+++ Day 3/day_3.py
@@ -3,20 +3,31 @@ import re
 
 def add_muls(groups):
     total = 0
+    do_state = True
     for group in groups:
+        if group == "do()":
+            do_state = True
+            continue
+        elif group == "don't()":
+            do_state = False
+            continue
+
         cleaned_group: str = group[4:-1]
         split_group = cleaned_group.split(",")
-        total += int(split_group[0]) * int(split_group[1])
+        if do_state:
+            total += int(split_group[0]) * int(split_group[1])
 
     return total
 
 
 def parse_input(file):
-    groups = []
+
+    find_groups = r"(mul\(\d{0,3},\d{0,3}\)|do\(\)|don't\(\))"
+
     with open(file) as f:
-        for line in f:
-            mul = re.findall(r"mul\(\d+,\d+\)", line)
-            groups = groups + mul
+        content = f.read()
+        groups = re.findall(find_groups, content)
+
     print(add_muls(groups))
 
 
